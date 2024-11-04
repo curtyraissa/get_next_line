0238807c1e994448fe9c182d6b9c70ea96f9d3e5
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-11-04 12:04:41 +0000

    Add files via upload

---
+++ get_next_line_utils.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcurty-g <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 08:06:32 by rcurty-g          #+#    #+#             */
+/*   Updated: 2024/11/04 11:05:33 by rcurty-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str && str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s != (char)c)
+	{
+		if (*s == '\0')
+			return (NULL);
+		s++;
+	}
+	return ((char *)s);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*joined_str;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	joined_str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!joined_str)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		joined_str[i] = s1[i];
+	j = 0;
+	while (s2[j])
+		joined_str[i++] = s2[j++];
+	joined_str[i] = '\0';
+	free(s1);
+	return (joined_str);
+}
